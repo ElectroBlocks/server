438d85bba241488964b51001bf73efb482bb16f1
Noah Glaser <glaserpower@gmail.com>
2020-11-16 22:18:02 -0800

    forgot cli

--- scripts/install-adafruit-boards.sh
+++ scripts/install-adafruit-boards.sh
@@ -1,3 +1,3 @@
-arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
+arduino-cli --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json" --save-prefs
 
 arduino-cli core install adafruit:avr
