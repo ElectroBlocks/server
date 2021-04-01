709d0289d0d1675cedcae13967d1798829d46a01
Noah Glaser <glaserpower@gmail.com>
2021-04-01 14:16:11 -0700

    adding digital display

---
+++ scripts/add-digital-display.sh
@@ -0,0 +1,5 @@
+## Add Digital Display
+mkdir /root/Arduino/libraries/arduino-tm1637
+cd /root/Arduino/libraries/arduino-tm1637
+git clone https://github.com/bremme/arduino-tm1637 .
+cd /root/electroblocks
